ab8621c82f874482eaefde87442909710bfc8b67
mojzu <mail@mojzu.net>
2017-08-29 22:43:10 +0100

    chore: style development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "AppRoot",
   templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  public title = "MojzuNet";
-}
+export class AppComponent { }
