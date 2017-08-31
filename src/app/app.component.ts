f2ec2aa8df9bd1a65a25cb39eb7ded5592dcea0d
mojzu <mail@mojzu.net>
2017-08-31 22:08:32 +0100

    chore: style development

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,5 @@ import { Component } from "@angular/core";
 @Component({
   selector: "AppRoot",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent { }
