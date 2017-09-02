36b5f0217e8bf893d8778e97a3285298d7dbe8b6
mojzu <mail@mojzu.net>
2017-09-02 01:04:43 +0100

    chore: style development

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule } from "@angular/flex-layout";
-import { MdToolbarModule } from "@angular/material";
+import { MdToolbarModule, MdGridListModule } from "@angular/material";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -16,6 +16,7 @@ import { AppComponent } from "./app.component";
     BrowserAnimationsModule,
     FlexLayoutModule,
     MdToolbarModule,
+    MdGridListModule,
     AppRoutingModule,
   ],
   providers: [],
