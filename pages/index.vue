16f44936b371580d5a48e53782d00aabaa2ed028
mojzu <mail@mojzu.net>
2017-12-03 16:33:13 +0000

    chore: remove nuxt

--- pages/index.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Mojzu</h1>
-</template>
--- plugins/semantic-ui-vue.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue';
-
-if (process.BROWSER_BUILD) {
-  window.onNuxtReady(() => {
-    const SuiVue = requre("semantic-ui-vue");
-    Vue.use(SuiVue);
-  });
-}
